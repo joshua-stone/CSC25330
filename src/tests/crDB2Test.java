3a443c039677335f6d00614fa5d5e6a7d8be4648
Joshua Stone <joshua.gage.stone@gmail.com>
2017-09-15 23:21:14 -0400

    Refactor tests with reusable code

--- src/tests/crDB2Test.java
+++ src/tests/crDB2Test.java
@@ -4,58 +4,43 @@ import crDB2.Rectangle;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-interface DEFAULTS {
-    double length = 1.0;
-    double width = 1.0;
-    double perimeter = 2 * (length + width);
-    double area = length * width;
-    String output = "Area 1.00, perimeter: 4.00";
+interface Defaults {
+    double LENGTH = 1.0;
+    double WIDTH = 1.0;
+    double PERIMETER = 2 * (LENGTH + WIDTH);
+    double AREA = LENGTH * WIDTH;
+    String OUTPUT = "Area 1.00, perimeter: 4.00";
 }
 
 class crDB2Test {
     // Ensure that the default constructor produces the expected values
     @Test
     void rectangleDefaultValues() {
-        Rectangle rect = new Rectangle();
-
-        assertEquals(DEFAULTS.length, rect.getLength());
-
-        assertEquals(DEFAULTS.width, rect.getWidth());
-
-        assertEquals(DEFAULTS.perimeter, rect.getPerimeter());
-
-        assertEquals(DEFAULTS.area, rect.getArea());
-
-        assertEquals(DEFAULTS.output, rect.toString());
+        this.defaultValueTest(new Rectangle());
     }
     // Show that passing 1.0 for length and width in constructor is equivalent to default constructor
     @Test
     void rectangleSetValues() {
-        Rectangle rect = new Rectangle(1.0, 1.0);
-
-        assertEquals(DEFAULTS.length, rect.getLength());
-
-        assertEquals(DEFAULTS.width, rect.getWidth());
-
-        assertEquals(DEFAULTS.perimeter, rect.getPerimeter());
-
-        assertEquals(DEFAULTS.area, rect.getArea());
-
-        assertEquals(DEFAULTS.output, rect.toString());
+        this.defaultValueTest(new Rectangle(1.0, 1.0));
     }
     // See if using values larger than 20 will set the value to defaults
     @Test
     void rectangleLargeValues() {
-        Rectangle rect = new Rectangle(21.0, 21.0);
+        this.defaultValueTest(new Rectangle(21.0, 21.0));
 
-        assertEquals(DEFAULTS.length, rect.getLength());
+        this.defaultValueTest(new Rectangle(21.0, 1.0));
 
-        assertEquals(DEFAULTS.width, rect.getWidth());
+        this.defaultValueTest(new Rectangle(1.0, 21.0));
+    }
+    void defaultValueTest(Rectangle rect) {
+        assertEquals(Defaults.LENGTH, rect.getLength());
 
-        assertEquals(DEFAULTS.perimeter, rect.getPerimeter());
+        assertEquals(Defaults.WIDTH, rect.getWidth());
 
-        assertEquals(DEFAULTS.area, rect.getArea());
+        assertEquals(Defaults.PERIMETER, rect.getPerimeter());
 
-        assertEquals(DEFAULTS.output, rect.toString());
+        assertEquals(Defaults.AREA, rect.getArea());
+
+        assertEquals(Defaults.OUTPUT, rect.toString());
     }
 }
