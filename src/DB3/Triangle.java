c0cc757907c27118f186557a6331e6468dcfa3f7
Joshua Stone <joshua.gage.stone@gmail.com>
2017-10-05 18:44:26 -0400

    Initial DB3 commit

---
+++ src/DB3/Triangle.java
@@ -0,0 +1,12 @@
+package DB3;
+
+public class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(final double side1, final double side2, final double side3) {
+
+    }
+    
+}
