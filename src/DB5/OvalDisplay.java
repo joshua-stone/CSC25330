e978abe2539524881ec591fdddbd11d73df0da36
Joshua Stone <joshua.gage.stone@gmail.com>
2017-11-12 19:56:04 -0500

    Create initial class

---
+++ src/DB5/OvalDisplay.java
@@ -0,0 +1,20 @@
+/*
+   Program: OvalDisplay.java
+   Written by: Joshua Stone
+   Description:
+   Challenges:
+   Time Spent:
+
+   Revision History:
+   Date:        By:             Action:
+   ---------------------------------------------------
+   11/12/17     Joshua Stone    Initial commit
+*/
+
+package DB5;
+
+public class OvalDisplay {
+    public static void main(final String args[]) {
+
+    }
+}
