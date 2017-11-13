2b5b88979c58f25e900b1feadf791dbad315f674
Joshua Stone <joshua.gage.stone@gmail.com>
2017-11-13 07:16:33 -0500

    Refactor class to use JPanel instead of JFrame

--- src/DB5/OvalDisplay.java
+++ src/DB5/OvalDisplay.java
@@ -9,12 +9,31 @@
    Date:        By:             Action:
    ---------------------------------------------------
    11/12/17     Joshua Stone    Initial commit
+   11/12/17     Joshua Stone    Add x, y, width, and height variables
+   11/12/17     Joshua Stone    Create input dialog function
 */
 
 package DB5;
 
-public class OvalDisplay {
-    public static void main(final String args[]) {
+import javax.swing.*;
 
+public class OvalDisplay extends JFrame {
+    private final double x;
+    private final double y;
+    private final double width;
+    private final double height;
+
+    public OvalDisplay() {
+        this.x = this.getDouble("Enter upper left X");
+        this.y = this.getDouble("Enter upper left Y");
+        this.width = this.getDouble("Enter width");
+        this.height = this.getDouble("Enter height");
+    }
+    private double getDouble(final String input) {
+        return Double.parseDouble(JOptionPane.showInputDialog(this, input));
+    }
+    public static void main(final String args[]) {
+        OvalDisplay ovalDisplay = new OvalDisplay();
+        ovalDisplay.setVisible(true);
     }
 }
